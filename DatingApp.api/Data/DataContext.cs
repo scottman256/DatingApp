76554e485b9c8042ecf48db1e5fdd6cc80112e27
Scott hall <scottmartinhall@gmail.com>
2020-04-17 22:24:37 -0400

    Initial Commit

---
+++ DatingApp.api/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using DatingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
